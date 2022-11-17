c3748d77c110fe6d0ab639c6c78f9769594eca86
RyanLY_Wang <RyanLY_Wang@asus.com>
2022-11-17 18:12:38 +0800

    [UART] Update comport_stress_test.sh
    Change-Id: I088136cb5873ddf0dc23bda81e05838a31d379c4
    
    Former-commit-id: d985077a0865db3b5785b6cd2db47a05472c24d7

--- overlay-debug/imx8mq/imx8mq-pv100a/home/root/Test_tool/comport_stress_test.sh
+++ overlay-debug/imx8mq/imx8mq-pv100a/home/root/Test_tool/comport_stress_test.sh
@@ -2,8 +2,15 @@
 
 now="$(date +'%Y%m%d_%H%M')"
 logfile="$1/$now"_mcu_uart.txt
-
+tmpfile=$1/com1_com2_tmp.txt
+COMPORT_TEST="/MCU_test_tool/comport_test"
 uart_err_cnt=0
+baud=9600
+str1=12345678
+str2=87654321
+err_cnt_12=0
+err_cnt_21=0
+i=0
 
 select_test_item()
 {
@@ -68,69 +75,144 @@ ex_gpio -c $COM1 $mode > /dev/null 2>&1
 ex_gpio -c $COM2 $mode > /dev/null 2>&1
 
 if [ "$COM1" == "3" ] || [ "$COM1" == "4" ]; then
-	comport_test_pv100a -c $COM1 -s 0 > /dev/null 2>&1
-	comport_test_pv100a -c $COM1 -s 1 -m $mode > /dev/null 2>&1
+	$COMPORT_TEST -c $COM1 -s 0 > /dev/null 2>&1
+	$COMPORT_TEST -c $COM1 -s 1 -m $mode -b $baud > /dev/null 2>&1
 fi
 
 if [ "$COM2" == "3" ] || [ "$COM2" == "4" ]; then
-	comport_test_pv100a -c $COM2 -s 0 > /dev/null 2>&1
-	comport_test_pv100a -c $COM2 -s 1 -m $mode > /dev/null 2>&1
+	$COMPORT_TEST -c $COM2 -s 0 > /dev/null 2>&1
+	$COMPORT_TEST -c $COM2 -s 1 -m $mode -b $baud > /dev/null 2>&1
 fi
 
-if [ "$mode" == "485" ]; then
-	echo "155" > /sys/class/gpio/export
-	echo "out" > /sys/class/gpio/gpio155/direction
+if [ "$COM1" == "1" ]; then
+	COM1PORT=/dev/ttymxc0
+elif [ "$COM1" == "2" ]; then
+	COM1PORT=/dev/ttymxc1
+fi
+
+if [ "$COM2" == "1" ]; then
+	COM2PORT=/dev/ttymxc0
+elif [ "$COM2" == "2" ]; then
+	COM2PORT=/dev/ttymxc1
+fi
+
+if [ $COM1PORT ]; then
+	serial-setup -c $COM1PORT -b $baud
+fi
+
+if [ $COM2PORT ]; then
+	serial-setup -c $COM2PORT -b $baud
 fi
 
 while [ 1 != 2 ]
 do
-	if [ "$mode" == "485" ] && [ "$COM1" == "1" ]; then
-		if [ "$rs485_mode" == "1" ]; then #DE
-			echo "0" > /sys/class/gpio/gpio155/value
-		else #RE
-			echo "1" > /sys/class/gpio/gpio155/value
+	echo "=========== $i loop ============="
+
+	if [ "$COM1" == "1" ] && [ "$COM2" == "2" ]; then	# SoC to SoC
+		# COM1 -> COM2
+		cat $COM2PORT > $tmpfile &
+		sleep 0.1
+		echo -e "$str1" > $COM1PORT
+		sleep 0.6
+		pid=$(ps -aux | grep $COM2PORT | grep -v grep | awk '{print $2}')
+		kill -9 $pid
+		result=$(cat $tmpfile | tee -a $logfile | grep $str1 )
+		rm $tmpfile
+		if [[ -n "$result" ]]; then
+			echo "COM$COM1 -> COM$COM2 Pass" | tee $2 $logfile
+			err_cnt_12=0
+		else
+			((err_cnt_12+=1))
+			echo "COM$COM1 -> COM$COM2 Fail, err_cnt_12=${err_cnt_12}" | tee $2 $logfile
+
+		fi
+
+		# COM2 -> COM1
+		cat $COM1PORT > $tmpfile &
+		sleep 0.1
+		echo -e "$str2" > $COM2PORT
+		sleep 0.5
+		pid=$(ps -aux | grep $COM1PORT | grep -v grep | awk '{print $2}')
+		kill -9 $pid
+		result=$(cat $tmpfile | tee -a $logfile | grep $str2 )
+		rm $tmpfile
+		if [[ -n "$result" ]]; then
+			echo "COM$COM2 -> COM$COM1 Pass" | tee $2 $logfile
+			err_cnt_21=0
+		else
+			((err_cnt_21+=1))
+			echo "COM$COM2 -> COM$COM1 Fail, err_cnt_21=${err_cnt_21}" | tee $2 $logfile
+		fi
+
+	elif [ "$COM1" == "3" ] && [ "$COM2" == "4" ]; then	# MCU to MCU
+		# COM1 -> COM2
+		$COMPORT_TEST -c $COM1 -w $str1 > /dev/null 2>&1
+		sleep 0.5
+		result=$($COMPORT_TEST -c $COM2 -r | grep $str1)
+		if [[ -n "$result" ]]; then
+			echo "COM$COM1 -> COM$COM2 Pass" | tee $2 $logfile
+			err_cnt_12=0
+		else
+			((err_cnt_12+=1))
+			echo "COM$COM1 -> COM$COM2 Fail, err_cnt_12=${err_cnt_12}" | tee $2 $logfile
+		fi
+
+		# COM2 -> COM1
+		$COMPORT_TEST -c $COM2 -w $str2 > /dev/null 2>&1
+		sleep 0.5
+		result=$($COMPORT_TEST -c $COM1 -r | grep $str2)
+		if [[ -n "$result" ]]; then
+			echo "COM$COM2 -> COM$COM1 Pass" | tee $2 $logfile
+			err_cnt_21=0
+		else
+			((err_cnt_21+=1))
+			echo "COM$COM2 -> COM$COM1 Fail, err_cnt_21=${err_cnt_21}" | tee $2 $logfile
+		fi
+
+	else							# SoC to MCU
+		# COM1 -> COM2
+		$COMPORT_TEST -c $COM2 -R > $tmpfile &
+		sleep 0.1
+		echo -e "$str1" > $COM1PORT
+		sleep 0.5
+		pid=$(ps -aux | grep "comport_test -c $COM2 -R" | grep -v grep | awk '{print $2}')
+		kill -9 $pid
+		result=$(cat $tmpfile | tee -a $logfile | grep $str1 )
+		rm $tmpfile
+		if [[ -n "$result" ]]; then
+			echo "COM$COM1 -> COM$COM2 Pass" | tee $2 $logfile
+			err_cnt_12=0
+		else
+			((err_cnt_12+=1))
+			echo "COM$COM1 -> COM$COM2 Fail, err_cnt_12=${err_cnt_12}" | tee $2 $logfile
+		fi
+
+		# COM2 -> COM1
+		cat $COM1PORT > $tmpfile &
+		sleep 0.1
+		$COMPORT_TEST -c $COM2 -w $str2 > /dev/null 2>&1
+		sleep 0.5
+		pid=$(ps -aux | grep $COM1PORT | grep -v grep | awk '{print $2}')
+		kill -9 $pid
+		result=$(cat $tmpfile | tee -a $logfile | grep $str2 )
+		if [[ -n "$result" ]]; then
+			echo "COM$COM2 -> COM$COM1 Pass" | tee $2 $logfile
+			err_cnt_21=0
+		else
+			((err_cnt_21+=1))
+			echo "COM$COM2 -> COM$COM1 Fail, err_cnt_21=${err_cnt_21}" | tee $2 $logfile
 		fi
 	fi
 
-	echo "COM$COM1 write" | tee $2 $logfile
-	comport_test_pv100a -c $COM1 -w "1234321 " > /dev/null 2>&1 &
-	sleep 0.1
-	result=$(comport_test_pv100a -c $COM2 -r | grep "1234321")
-	if [[ -n "$result" ]]; then
-		echo "COM$COM2 read Pass" | tee $2 $logfile
-		uart_err_cnt=0
-	else
-		echo "COM$COM2 read Fail" | tee $2 $logfile
-		((uart_err_cnt+=1))
-	fi
-	echo "" | tee $2 $logfile
-
-######################################################################
-
-	if [ "$mode" == "485" ] && [ "$COM1" == "1" ]; then
-		if [ "$rs485_mode" == "1" ]; then #DE
-			echo "1" > /sys/class/gpio/gpio155/value
-		else #RE
-			echo "0" > /sys/class/gpio/gpio155/value
-		fi
+	if [[ ${err_cnt_12} -eq 3 || ${err_cnt_21} -eq 3 ]]; then
+		sleep 3
 	fi
 
-	echo "COM$COM2 write" | tee $2 $logfile
-	comport_test_pv100a -c $COM2 -w "4321234 " > /dev/null 2>&1 &
-	sleep 0.1
-	result=$(comport_test_pv100a -c $COM1 -r | grep "4321234")
-	if [[ -n "$result" ]]; then
-		echo "COM$COM1 read Pass" | tee $2 $logfile
-		uart_err_cnt=0
-	else
-		echo "COM$COM1 read Fail" | tee $2 $logfile
-		((uart_err_cnt+=1))
-	fi
-	echo "" | tee $2 $logfile
-	if [[ $uart_err_cnt -ge 5 ]]; then
-		echo "$(date +'%Y%m%d_%H%M') UART continue fail over 5 times, uart_err_cnt=$uart_err_cnt" | tee $2 $logfile
-		comport_test_pv100a -s 0 > /dev/null 2>&1
+	if [[ ${err_cnt_12} -ge 5 || ${err_cnt_21} -ge 5 ]]; then
+		echo "COM$COM1 <-> COM$COM2 continue fail over 5 times, err_cnt_12=${err_cnt_12}, err_cnt_21=${err_cnt_21}" | tee $2 $logfile
 		exit 1
 	fi
+	sleep 0.1
+	((i+=1))
 done
 
