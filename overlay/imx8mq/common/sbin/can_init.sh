30e159cb24284808d8d712e3d5a245b1eab972a3
Kunyang_Fan <Kunyang_Fan@asus.com>
2022-12-09 13:34:26 +0800

    Can: Init can device in bootup instead of udev
    Change-Id: I0353833e8e29f8efd4cac4eca3c88986a9017253
    Signed-off-by: Kunyang_Fan <Kunyang_Fan@asus.com>
    
    Former-commit-id: 57091d2862d3dc43edb46a20fb71aaf8104e0229

--- overlay/imx8mq/common/sbin/can_init.sh
+++ overlay/imx8mq/common/sbin/can_init.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
-iface=$1
-counter=0
-while [ $counter -le 3 ]
-do
-	/sbin/ip link set $iface up type can bitrate 125000
-	if [[ $( ifconfig -s | grep $iface ) ]]; then
-		echo "$iface is up!"
-		exit
-	fi
-	echo "$iface is up fail, rety again"
-	counter=`expr $counter + 1`
-done
+function enable_can()
+{
+	iface="$1"
+	counter=0
+	echo "$iface"
+	while [ $counter -le 3 ]
+	do
+		/sbin/ip link set $iface up type can bitrate 125000
+		check_val=$( /sbin/ifconfig -s | grep "$iface" )
+		if [ ! -z "$check_val" ]; then
+			echo "$iface is up!"
+			return
+		fi
+		echo "$iface is up fail, rety again"
+		counter=`expr $counter + 1`
+	done
+	echo "$iface is up fail over 3 times"
+}
 
-echo "$iface is up fail over 3 times"
+if [ ! -z $( ls /sys/class/net | grep can0 ) ]; then
+	echo "enable can0"
+	enable_can "can0"
+fi
+
+if [ ! -z $( ls /sys/class/net | grep can1 ) ]; then
+	echo "enable can1"
+	enable_can "can1"
+fi
+
+
