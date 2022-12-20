7698bba888426f948ff9d285941bc843b076e2e3
tzuwen_chang <TzuWen_Chang@asus.com>
2022-12-20 10:50:50 +0800

    test tools: Modify Storage benchmark test count to 15
    Change-Id: Idb5d7cb3a25603bd2aa7d96d524c70cfe3cdee46
    
    Former-commit-id: bd984d41cf5224860e761e9e11a87729212875bd

--- overlay-debug/imx8mq/common/home/root/Test_tool/Storage_benchmark_test/Storage_benchmark_test.sh
+++ overlay-debug/imx8mq/common/home/root/Test_tool/Storage_benchmark_test/Storage_benchmark_test.sh
@@ -18,10 +18,11 @@ SCRIPTPATH=`dirname $SCRIPT`
 info_view
 
 echo "Start Write Test"
-sudo dd if=/dev/zero of=$test_path/tmpfile bs=256M count=2 conv=fdatasync
+echo 3 | sudo tee /proc/sys/vm/drop_caches
+sudo dd if=/dev/zero of=$test_path/tmpfile bs=256M count=15 conv=fdatasync
 
 echo "Start Read Test"
 echo 3 | sudo tee /proc/sys/vm/drop_caches
-sudo dd if=$test_path/tmpfile of=/dev/null bs=256M count=2
+sudo dd if=$test_path/tmpfile of=/dev/null bs=256M count=15
 sudo rm $test_path/tmpfile
 
